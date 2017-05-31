a0104036b803685c7c39ada7ae415303534ccd9b
German Mazza <ger.mazzagentile@gmail.com>
2017-05-30 23:09:54 -0300

    Correccion en el calculo de lugares cercanos

--- app/Repositories/PlacesRepository.php
+++ app/Repositories/PlacesRepository.php
@@ -169,10 +169,10 @@ class PlacesRepository
             'user_id as userId',
             'visible',
             'address',
-            DB::raw('(3959 * acos( cos( radians(37) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(-122) ) + sin( radians(37) ) * sin(radians(latitude)) ) ) AS distance')
+            DB::raw('(6351 * acos( cos( radians('.$latitude.') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('.$langitude.') ) + sin( radians('.$latitude.') ) * sin(radians(latitude)) ) ) AS distance')
         ])
             ->orderBy('distance')
-            ->havingRaw('distance < 500')
-            ->get()->take(20);
+            ->havingRaw('distance < ' . env("PLACES_SEARCH_DISTANCE"))
+            ->get()->take(env("LIMIT_SEARCH_DISTANCE"));
     }
 }
